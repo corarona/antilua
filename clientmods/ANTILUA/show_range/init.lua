34836948b91a9cec02ad463ec33f225caec93cc4
cora <coradelamouche@gmx.ch>
2026-07-19 23:36:42 +0200

    ShowRange: add debug logging for actual r value

--- clientmods/ANTILUA/show_range/init.lua
+++ clientmods/ANTILUA/show_range/init.lua
@@ -1,5 +1,4 @@
 local GROUP = 9999
-local DEBUG_GROUP = 9998
 
 ws.rg("ShowRange", {
 	category = "Render",
@@ -10,22 +9,19 @@ ws.rg("ShowRange", {
 	},
 	on_step = function()
 		core.draw3d:clear(GROUP)
-		core.draw3d:clear(DEBUG_GROUP)
 
 		local pos = core.localplayer and core.localplayer:get_pos()
 		if not pos then return end
 
+		-- Read from setting or default to 6.6
 		local r = tonumber(core.settings:get("show_range.range")) or 6.6
 
+		core.log("action", "[ShowRange] r=" .. tostring(r) ..
+			" pos=" .. core.pos_to_string(pos))
+
 		core.draw3d:add_wiresphere(pos, r, "#FFFFFFFF", 48, GROUP)
-		core.draw3d:add_wirebox(
-			{x = pos.x - r, y = pos.y - r, z = pos.z - r},
-			{x = pos.x + r, y = pos.y + r, z = pos.z + r},
-			"#FF0000", DEBUG_GROUP
-		)
 	end,
 	on_stop = function()
 		core.draw3d:clear(GROUP)
-		core.draw3d:clear(DEBUG_GROUP)
 	end,
 })
