70a94a9d2e93279aedc7a20adc6f954c26af4900
cora <coradelamouche@gmx.ch>
2026-07-19 23:20:07 +0200

    ShowRange: fully opaque white, 48 segments for denser wireframe

--- clientmods/ANTILUA/show_range/init.lua
+++ clientmods/ANTILUA/show_range/init.lua
@@ -1,4 +1,5 @@
 local GROUP = 9999
+local DEBUG_GROUP = 9998
 
 ws.rg("ShowRange", {
 	category = "Render",
@@ -7,13 +8,16 @@ ws.rg("ShowRange", {
 	cheat_settings = {
 		range = { default = 6.6, min = 1, max = 50 },
 	},
-on_step = function()
-	core.draw3d:clear(GROUP)
-	local pos = core.localplayer and core.localplayer:get_pos()
-	if not pos then return end
-	local r = tonumber(core.settings:get("show_range.range")) or 6.6
-	core.draw3d:add_wiresphere(pos, r, "#FFFFFF80", 32, GROUP)
-end,
+	on_step = function()
+		core.draw3d:clear(GROUP)
+
+		local pos = core.localplayer and core.localplayer:get_pos()
+		if not pos then return end
+
+		local r = tonumber(core.settings:get("show_range.range")) or 6.6
+
+		core.draw3d:add_wiresphere(pos, r, "#FFFFFFFF", 48, GROUP)
+	end,
 	on_stop = function()
 		core.draw3d:clear(GROUP)
 	end,
