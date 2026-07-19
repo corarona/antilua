72b30acbcd90efc51d0bae29e8e096cd3acb20a5
cora <coradelamouche@gmx.ch>
2026-07-19 23:21:14 +0200

    ShowRange: add matching red box to visually verify sphere radius

--- clientmods/ANTILUA/show_range/init.lua
+++ clientmods/ANTILUA/show_range/init.lua
@@ -10,6 +10,7 @@ ws.rg("ShowRange", {
 	},
 	on_step = function()
 		core.draw3d:clear(GROUP)
+		core.draw3d:clear(DEBUG_GROUP)
 
 		local pos = core.localplayer and core.localplayer:get_pos()
 		if not pos then return end
@@ -17,8 +18,14 @@ ws.rg("ShowRange", {
 		local r = tonumber(core.settings:get("show_range.range")) or 6.6
 
 		core.draw3d:add_wiresphere(pos, r, "#FFFFFFFF", 48, GROUP)
+		core.draw3d:add_wirebox(
+			{x = pos.x - r, y = pos.y - r, z = pos.z - r},
+			{x = pos.x + r, y = pos.y + r, z = pos.z + r},
+			"#FF0000", DEBUG_GROUP
+		)
 	end,
 	on_stop = function()
 		core.draw3d:clear(GROUP)
+		core.draw3d:clear(DEBUG_GROUP)
 	end,
 })
