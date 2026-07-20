f87f36a39a28c5aa2f750136e076b1659b3be2cb
cora <coradelamouche@gmx.ch>
2026-07-20 03:22:10 +0200

    ShowRange: add back 10-block reference line with ticks for accurate distance comparison

--- clientmods/ANTILUA/show_range/init.lua
+++ clientmods/ANTILUA/show_range/init.lua
@@ -1,4 +1,5 @@
 local GROUP = 9999
+local REF_GROUP = 9998
 
 ws.rg("ShowRange", {
 	category = "Render",
@@ -9,12 +10,27 @@ ws.rg("ShowRange", {
 	},
 	on_step = function()
 		core.draw3d:clear(GROUP)
+		core.draw3d:clear(REF_GROUP)
+
 		local pos = core.localplayer and core.localplayer:get_pos()
 		if not pos then return end
+
 		local r = tonumber(core.settings:get("show_range.range")) or 6.6
+
+		-- Reference: 10-block line with tick marks
+		for i = 0, 10 do
+			core.draw3d:add_line(
+				{x = pos.x + i, y = pos.y - 0.3, z = pos.z},
+				{x = pos.x + i, y = pos.y + 0.3, z = pos.z},
+				"#FFFF00", REF_GROUP)
+		end
+		core.draw3d:add_line(pos, {x = pos.x + 10, y = pos.y, z = pos.z}, "#FF0000", REF_GROUP)
+
+		-- Range sphere
 		core.draw3d:add_wiresphere(pos, r, "#FFFFFF", 48, GROUP)
 	end,
 	on_stop = function()
 		core.draw3d:clear(GROUP)
+		core.draw3d:clear(REF_GROUP)
 	end,
 })
